8d7cc633a904b51476e254f5782defb7be3ee63c
Nebula <lazarjorgacevic936@gmail.com>
2026-09-14 22:03:13 +0200

    [feat] Added GetByAssociation to AnimalService

--- PetShelter/PetShelter.Application/Services/AnimalService.cs
+++ PetShelter/PetShelter.Application/Services/AnimalService.cs
@@ -1,10 +1,20 @@
+using PetShelter.Application.Domain;
+using PetShelter.Application.Domain.RepositoryInterfaces;
 using PetShelter.Application.Services.ServiceInterfaces;
 
 namespace PetShelter.Application.Services;
 
 public class AnimalService: IAnimalService
 {
+    private readonly IAnimalRepository _animalRepository;
     public  AnimalService()
     {
+        _animalRepository = Injector.CreateInstance<IAnimalRepository>();
+    }
+
+    public List<Animal> GetByAssociation(long associationId)
+    {
+        var animals = _animalRepository.GetAll();
+        return animals.Where(a => a.AssociationId == associationId && a.IsDeleted == false).ToList();
     }
 }
--- PetShelter/PetShelter.Application/Services/ServiceInterfaces/IAnimalService.cs
+++ PetShelter/PetShelter.Application/Services/ServiceInterfaces/IAnimalService.cs
@@ -1,6 +1,8 @@
+using PetShelter.Application.Domain;
+
 namespace PetShelter.Application.Services.ServiceInterfaces;
 
 public interface IAnimalService
 {
-    
+    public List<Animal> GetByAssociation(long associationId);
 }

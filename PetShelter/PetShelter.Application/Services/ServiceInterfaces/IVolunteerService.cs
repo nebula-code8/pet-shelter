bba9c1bdbf245b8edb1e9bcbf439025363321df5
DmitrNKL <work.hiorolink.21@gmail.com>
2026-09-15 15:29:28 +0200

    Add volunteer service

--- PetShelter/PetShelter.Application/Services/ServiceInterfaces/IVolunteerService.cs
+++ PetShelter/PetShelter.Application/Services/ServiceInterfaces/IVolunteerService.cs
@@ -1,6 +1,8 @@
+using PetShelter.Application.Domain;
+
 namespace PetShelter.Application.Services.ServiceInterfaces;
 
 public interface IVolunteerService
 {
-    
+    long Insert(Volunteer volunteer);
 }
--- PetShelter/PetShelter.Application/Services/VolunteerService.cs
+++ PetShelter/PetShelter.Application/Services/VolunteerService.cs
@@ -1,11 +1,43 @@
+using PetShelter.Application.Domain;
+using PetShelter.Application.Domain.RepositoryInterfaces;
 using PetShelter.Application.Services.ServiceInterfaces;
 
 namespace PetShelter.Application.Services;
 
-public class VolunteerService: IVolunteerService
+public class VolunteerService : IVolunteerService
 {
+    private readonly IVolunteerRepository _volunteerRepository;
+
     public VolunteerService()
     {
-        
+        _volunteerRepository = Injector.CreateInstance<IVolunteerRepository>();
+    }
+
+    public long Insert(Volunteer volunteer)
+    {
+        Validate(volunteer);
+
+        return _volunteerRepository.Insert(volunteer);
+    }
+
+    private static void Validate(Volunteer volunteer)
+    {
+        if (string.IsNullOrWhiteSpace(volunteer.Name) ||
+            string.IsNullOrWhiteSpace(volunteer.Surname) ||
+            string.IsNullOrWhiteSpace(volunteer.PhoneNumber) ||
+            string.IsNullOrWhiteSpace(volunteer.EmailAddress) ||
+            string.IsNullOrWhiteSpace(volunteer.Password) ||
+            string.IsNullOrWhiteSpace(volunteer.Address) ||
+            string.IsNullOrWhiteSpace(volunteer.VolunteerComment))
+        {
+            throw new Exception("Please fill in all fields.");
+        }
+
+        if (volunteer.AssociationId <= 0)
+        {
+            throw new Exception(
+                "Please select an association."
+            );
+        }
     }
 }
